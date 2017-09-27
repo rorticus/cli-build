2e486718850ca1bb6c23b0d150b41a9469eb59ef
Matt Gadd <mgadd@sitepen.com>
2017-09-27 16:38:24 +0100

    change output

--- src/config/app/dev.ts
+++ src/config/app/dev.ts
@@ -1,9 +1,11 @@
 import { BuildArgs } from '../../main';
 import baseConfig from './base';
+import * as path from 'path';
 
 function webpackConfig(args: Partial<BuildArgs>) {
 	const config: any = baseConfig(args);
 	config.devtool = 'inline-source-map';
+	config.output.path = path.join(config.output.path, 'dev');
 	return config;
 }
 
