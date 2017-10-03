24caff108ed1ee81b12be696171f767df849ed4a
Matt Gadd <mgadd@sitepen.com>
2017-10-03 15:37:43 +0100

    btr

--- src/config/app/base.ts
+++ src/config/app/base.ts
@@ -1,6 +1,6 @@
 import webpack = require('webpack');
 import Set from '@dojo/shim/Set';
-import { existsSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import * as path from 'path';
 import { BuildArgs } from '../../main';
 
@@ -93,6 +93,67 @@ function getCSSModuleLoader() {
 
 const removeEmpty = (items: any[]) => items.filter((item) => item);
 
+class BuildTimeRender {
+	private _paths: any;
+
+	constructor(args = { paths: [] }) {
+		this._paths = args.paths;
+	}
+
+	apply(compiler: webpack.Compiler) {
+		compiler.plugin('done', () => {
+			const buildTag = '<!-- btr -->';
+			const output = compiler.options.output && compiler.options.output.path || basePath;
+			let html = readFileSync(path.join(output, 'index.html'), 'utf-8');
+			if (html.indexOf(buildTag) === -1) { return; }
+			const cleanup = require('jsdom-global')();
+			const filterCss = require('filter-css');
+			(global as any).window.DojoHasEnvironment = {
+				staticFeatures: {
+					'build-time-render': true
+				}
+			};
+			(global as any).Element = function() {};
+			(global as any).requestAnimationFrame = () => {};
+			(global as any).cancelAnimationFrame = () => {};
+			(global as any).IntersectionObserver = () => {};
+			require(path.join(output, mainEntry));
+
+			let classes: string[] = [];
+
+			const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_ELEMENT);
+
+			while (treeWalker.nextNode()) {
+				const node: any = treeWalker.currentNode;
+				node.classList.length && classes.push.apply(classes, node.classList);
+			}
+
+			classes = classes.map((className) => `.${className}`);
+
+			const result = filterCss(path.join(output, mainEntry + '.css'), (context: any, value: any, node: any) => {
+				if (context === 'selector') {
+					value = value.replace(/(:| ).*/, '');
+					const firstChar = value.substr(0, 1);
+					if (classes.indexOf(value) !== -1 || [ '.', '#' ].indexOf(firstChar) === -1) {
+						return false;
+					}
+					return true;
+				}
+			}).replace(/\/\*# sourceMappingURL\=.*/, '');
+
+			const replacement = `
+${document.body.innerHTML}
+<link rel="stylesheet" href="${mainEntry}.css" media="none" onload="if(media!='all')media='all'">
+`;
+
+			html = html.replace(`<link href="${mainEntry}.css" rel="stylesheet">`, `<style>${result}</style>`);
+			html = html.replace(buildTag, replacement);
+			writeFileSync(path.join(output, 'index.html'), html);
+			cleanup();
+		});
+	}
+}
+
 function webpackConfig(args: Partial<BuildArgs>) {
 	args = args || {};
 	const serviceWorker = args.pwa && args.pwa.serviceWorker && {
@@ -105,9 +166,10 @@ function webpackConfig(args: Partial<BuildArgs>) {
 	const config: webpack.Configuration = {
 		entry: {
 			[ mainEntry ]: removeEmpty([
+				serviceWorker && path.join(__dirname, 'sw.js'),
+				path.join(__dirname, 'btr.js'),
 				path.join(srcPath, 'main.css'),
-				path.join(srcPath, 'main.ts'),
-				serviceWorker && path.join(__dirname, 'sw.js')
+				path.join(srcPath, 'main.ts')
 			])
 		},
 		node: { dgram: 'empty', net: 'empty', tls: 'empty', fs: 'empty' },
@@ -123,7 +185,8 @@ function webpackConfig(args: Partial<BuildArgs>) {
 			new ExtractTextPlugin({ filename: 'src/main.css', allChunks: true }),
 			new HtmlWebpackPlugin({ inject: 'body', chunks: [ mainEntry ], template: path.join(srcPath, 'index.html') }),
 			serviceWorker && new OfflinePlugin(serviceWorker),
-			manifest && new WebpackPwaManifest(manifest)
+			manifest && new WebpackPwaManifest(manifest),
+			new BuildTimeRender()
 		]),
 		output: {
 			chunkFilename: '[name].js',
@@ -139,7 +202,9 @@ function webpackConfig(args: Partial<BuildArgs>) {
 		},
 		devtool: 'source-map',
 		watchOptions: { ignored: /node_modules/ },
-		resolve: { extensions: ['.ts', '.tsx', '.js'] },
+		resolve: {
+			modules: [ basePath, path.join(basePath, 'node_modules') ],
+			extensions: ['.ts', '.tsx', '.js'] },
 		resolveLoader: { modules: [ path.join(__dirname, '../../loaders'), path.join(__dirname, '../../node_modules'), 'node_modules' ] },
 		module: {
 			rules: removeEmpty([
